5e1309d19efb4d187a452db394af5daa87f90efd
Abbas463 <abbashilal463@gmail.com>
2026-02-17 10:25:01 +0430

    Initial commit

--- main.py
+++ main.py
@@ -4,4 +4,13 @@ win=tk.Tk()
 win.title('GUI')
 win.geometry('100x200')
 
+button1=tk.Button(win, text="Button1")
+button1.pack()
+
+button2=tk.Button(win, text="Button2")
+button2.pack()
+
+button3=tk.Button(win, text="Button3")
+button3.pack()
+
 win.mainloop()
