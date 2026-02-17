641b40f4a3ed742ca100e0aca0d053b46a21fa15
Abbas463 <abbashilal463@gmail.com>
2026-02-17 10:58:36 +0430

    Initial commit

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ app = tk.Tk()
 style = Style(theme = "darkly")
 app.title('GUI')
 app.geometry('100x100')
+app.state('zoomed')
 
 button1 = ttkb.Button(app, text = "Button1", bootstyle = 'info')
 button1.pack()
