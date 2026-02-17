6b75732bcf10833d4e34101e982755e77a63b597
Abbas463 <abbashilal463@gmail.com>
2026-02-17 10:33:43 +0430

    Initial commit

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 import tkinter as tk
 from ttkbootstrap import Style
 
-win=tk.Tk()
+app=tk.Tk()
 style=Style(theme="darkly")
-win.title('GUI')
-win.geometry('100x100')
+app.title('GUI')
+app.geometry('100x100')
 
-button1=tk.Button(win, text="Button1")
+button1=tk.Button(app, text="Button1")
 button1.pack()
 
-button2=tk.Button(win, text="Button2")
+button2=tk.Button(app, text="Button2")
 button2.pack()
 
-button3=tk.Button(win, text="Button3")
+button3=tk.Button(app, text="Button3")
 button3.pack()
 
-win.mainloop()
+app.mainloop()
